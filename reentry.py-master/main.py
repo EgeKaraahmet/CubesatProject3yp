93f3c4a1c74dea17d822a56b2edec67a22bb90df
6HW6 <149905881+6HW6@users.noreply.github.com>
2023-11-13 20:35:37 +0000

    graph updates
    1) computed the maximum downrange, plot alt (km) against downrange (km)
    
    2) based on the maximum downrange, computed no. of completed Earth circumference, plot alt(km) against no. of completed Earth circumference
    
    3) Modified plot of axial load against time. Modified the constraint to the time axis so the peak is more visible.

--- reentry.py-master/main.py
+++ reentry.py-master/main.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 sim = {
     "max_it": 10000000,
     "delta_t": 1,
-    "entry_interface": 400e3,   # QARMAN mission
+    "entry_interface": 200e3,   # QARMAN mission
     "fpa": 0,
     "velocity": 7788,
     "stop_alt": 4e3
@@ -49,17 +49,31 @@ dtg = np.max(downrange) - downrange
 
 
 
-
+num_points = len(alt)
+downrange_flat_earth = np.linspace(0, num_points, num_points)
+no_of_Earth_circumference = downrange_flat_earth /(2*np.pi*planet.radius)
 
 
 # Create a single figure and multiple subplots
 fig, axs = plt.subplots(2, 3, figsize=(15, 7.5))  # 2 rows and 3 columns of subplots
 
 # Plot the data on each subplot
-do_plot(axs[0, 0], 'downrange (km)', downrange / 1e3, 'altitude (km)', alt / 1e3, label, title)
+do_plot(axs[0, 0], 'Downrange (km)', downrange_flat_earth / 1e3, 'altitude (km)', alt / 1e3, label, title)
+# do_plot(axs[0, 0], 'Downrange (km)', downrange / 1e3, 'altitude (km)', alt / 1e3, label, title)
+do_plot(axs[1, 0], 'No. of completed Earth Circumference', no_of_Earth_circumference, 'altitude (km)', alt / 1e3, label, title)
+# do_plot(axs[1, 0], 'distance to splashdown (km)', dtg / 1e3, 'time to parachute deploy (s)', tti / 60.0, label, title)
+
 do_plot(axs[0, 1], 'axial loads (g)', aa / 9.81, 'altitude (km)', alt / 1e3, label, title)
-do_plot(axs[0, 2], 'time since EI (s)', t/(86400), 'axial loads (g)', aa / 9.81, label, title)
-do_plot(axs[1, 0], 'distance to splashdown (km)', dtg / 1e3, 'time to parachute deploy (s)', tti / 60.0, label, title)
+
+# Limiting the axies
+ax = axs[0, 2]
+do_plot(ax, 'time since EI (days)', (t/(86400)), 'axial loads (g)', aa / 9.81, label, title)
+# Set the x-axis limits to start from t/86400 = 3
+min_x = 3.2
+max_x = max(t/86400)  # Calculate the maximum value dynamically
+ax.set_xlim(min_x, max_x)
+
+
 do_plot(axs[1, 1], 'velocity (km/s)', v / 1e3, 'altitude (km)', alt / 1e3, label, title)
 do_plot(axs[1, 2],'time (days)', t/(86400),'altitude (km)' , alt / 1e3, label, title)
 
