ecac2bd955a42a6dd51df4af86f467a24c9b1d41
EgeKaraahmet <EgeKaraahmet@gmail.com>
2024-01-29 13:14:04 +0000

    Added new variables to ATM1976 data file

--- Thermochemical codes/ATM1976.m
+++ Thermochemical codes/ATM1976.m
@@ -1,11 +1,15 @@
-function [T,P,rho] = ATM1976(Z)
+function [T,P,rho,grav_const,MolecularWeight] = ATM1976(Z)
 %ENTER ALTITUDE AS A MULTIPLE OF 500 IN METERS.
 %OUTPUTS TEMPERATURE,PRESSURE,DENSITY in SI UNITS.
 % addpath('/Users/zhanghanwen/Library/CloudStorage/OneDrive-Nexus365/3YP AtCUBESAT/Simulation/Gayhub/CubesatTrajectory/Thermochemical codes/Database')
-load("US1976_matlabdata.mat")
+
+
+load("Database/US1976_matlabdata.mat")
 
 index = find(Zm==Z);
 
 rho = rhokgm3(index);
 T = TK(index);
 P = PPa(index);
+grav_const = g(index);
+MolecularWeight = M(index);
