b7beb629b179d406b44a7010f6bef818c8801dcf
EgeKaraahmet <EgeKaraahmet@gmail.com>
2024-01-29 19:06:00 +0000

    ATM1976 now works with multiples of 500

--- Thermochemical codes/ATM1976.m
+++ Thermochemical codes/ATM1976.m
@@ -12,13 +12,18 @@ if mod(Z,500) ~= 0
     remainder = mod(Z,500);
     Z1 = Z - remainder;
     Z2 = Z1 + 500;
-
+    index1 = find(Zm==Z1);
+    index2 = find(Zm==Z2);
+    rho2 = rhokgm3(index2);
+    T2 = TK(index2);
+    P2 = PPa(index2);
+    grav_const2 = g(index2);
+    MolecularWeight2 = M(index2);
+else
+    Z1 = Z;
+    remainder = 0;
+    index1 = find(Zm==Z1);
 end
-
-
-index1 = find(Zm==Z1);
-index2 = find(Zm==Z2);
-
 
 
 
@@ -35,18 +40,18 @@ grav_const1 = g(index1);
 MolecularWeight1 = M(index1);
 
 
-rho2 = rhokgm3(index2);
-T2 = TK(index2);
-P2 = PPa(index2);
-grav_const2 = g(index2);
-MolecularWeight2 = M(index2);
 
 
-
-
-
-rho = ((1-weight)*rho1)+((weight)*rho2);
-T = ((1-weight)*T1+(weight)*T2);
-P = ((1-weight)*P1+(weight)*P2);
-grav_const = ((1-weight)*grav_const1 + (weight)*grav_const2);
-MolecularWeight = ((1-weight)*MolecularWeight1 + (weight)*MolecularWeight2);
+if mod(Z,500) ~= 0
+    rho = ((1-weight)*rho1)+((weight)*rho2);
+    T = ((1-weight)*T1+(weight)*T2);
+    P = ((1-weight)*P1+(weight)*P2);
+    grav_const = ((1-weight)*grav_const1 + (weight)*grav_const2);
+    MolecularWeight = ((1-weight)*MolecularWeight1 + (weight)*MolecularWeight2);
+else
+    rho = rhokgm3(index1);
+    T = TK(index1);
+    P = PPa(index1);
+    grav_const = g(index1);
+    MolecularWeight = M(index1);
+end
