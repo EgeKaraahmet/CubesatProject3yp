2434903adda7c06462b31a8e9931d5912fc54935
6HW6 <149905881+6HW6@users.noreply.github.com>
2024-02-29 16:38:40 +0000

    Post analysis
    New data set for perturbation test

--- Trajectory/MATLAB/MPC/perturbation_post_analysis.m
+++ Trajectory/MATLAB/MPC/perturbation_post_analysis.m
@@ -102,6 +102,22 @@ load('A126.mat')
 h_plot_kmf_A126 = xHistory_kmf(:,1);
 X_plot_kmf_A126 = xHistory_kmf(:,2);
 
+load('A126p5.mat')
+h_plot_kmf_A126p5 = xHistory_kmf(:,1);
+X_plot_kmf_A126p5 = xHistory_kmf(:,2);
+
+load('A127.mat')
+h_plot_kmf_A127 = xHistory_kmf(:,1);
+X_plot_kmf_A127 = xHistory_kmf(:,2);
+
+load('A127p5.mat')
+h_plot_kmf_A127p5 = xHistory_kmf(:,1);
+X_plot_kmf_A127p5 = xHistory_kmf(:,2);
+
+load('A128.mat')
+h_plot_kmf_A128 = xHistory_kmf(:,1);
+X_plot_kmf_A128 = xHistory_kmf(:,2);
+
 %%
 plot(abs(X_plot_kmf_A122)/1000,h_plot_kmf_A122/1000, ...
     abs(X_plot_kmf_A122p5)/1000,h_plot_kmf_A122p5/1000, ...
@@ -112,6 +128,10 @@ plot(abs(X_plot_kmf_A122)/1000,h_plot_kmf_A122/1000, ...
     abs(X_plot_kmf_A125)/1000,h_plot_kmf_A125/1000, ...
     abs(X_plot_kmf_A125p5)/1000,h_plot_kmf_A125p5/1000, ...
     abs(X_plot_kmf_A126)/1000,h_plot_kmf_A126/1000, ...
+    abs(X_plot_kmf_A126p5)/1000,h_plot_kmf_A126p5/1000, ...
+    abs(X_plot_kmf_A127)/1000,h_plot_kmf_A127/1000, ...
+    abs(X_plot_kmf_A127p5)/1000,h_plot_kmf_A127p5/1000, ...
+    abs(X_plot_kmf_A128)/1000,h_plot_kmf_A128/1000,...
     abs(x_reference_atm)/1000,h_reference_atm/1000,'b');
  
 
@@ -122,60 +142,85 @@ ylabel('Altitude (km)');
 legend('Reference altitude vs. distance ', 'Trajectory Tracking (with EKF)')
 
 % Add legend
-legend('A=22cm^2','A=22.5cm^2','A=23cm^2', 'A=23.5cm^2', 'A=24cm^2', 'A=24.5cm^2', 'A=25cm^2','A=25.5cm^2','A=26cm^2', 'Reference Altitude', 'Location', 'southwest');
+legend('A=22cm^2','A=22.5cm^2','A=23cm^2', 'A=23.5cm^2', 'A=24cm^2', 'A=24.5cm^2', 'A=25cm^2','A=25.5cm^2','A=26cm^2','A=26.5cm^2','A=27cm^2','A=27.5cm^2','A=28cm^2',  'Reference Altitude', 'Location', 'southwest');
 % Calculation 
 h_alt = 6378; 
 %%
 X_last_ref = abs(x_reference_atm(end))/1000; 
-X_last_ref = X_last_ref - (2*pi*h_alt)*floor(X_last_ref/(2*pi*h_alt));
+%X_last_ref = X_last_ref - (2*pi*h_alt)*floor(X_last_ref/(2*pi*h_alt));
 p_ref = sqrt(h_alt^2 + X_last_ref^2);
 
-%%
+
 X_last_123 = abs(X_plot_kmf_A123(end))/1000; 
-X_last_123 = X_last_123 - (2*pi*h_alt)*floor(X_last_123/(2*pi*h_alt));
+%X_last_123 = X_last_123 - (2*pi*h_alt)*floor(X_last_123/(2*pi*h_alt));
 p_123 = sqrt(h_alt^2 + X_last_123^2); 
 pe_123 = abs(p_123 - p_ref);
-%%
+
 X_last_123p5 = abs(X_plot_kmf_A123p5(end))/1000; 
-X_last_123p5 = X_last_123p5 - (2*pi*h_alt)*floor(X_last_123p5/(2*pi*h_alt));
+%X_last_123p5 = X_last_123p5 - (2*pi*h_alt)*floor(X_last_123p5/(2*pi*h_alt));
 p_123p5 = sqrt(h_alt^2 + X_last_123p5^2);
 pe_123p5 = abs(p_123p5 - p_ref);
 
 X_last_124 = abs(X_plot_kmf_A124(end))/1000; 
-X_last_124 = X_last_124 - (2*pi*h_alt)*floor(X_last_124/(2*pi*h_alt));
+%X_last_124 = X_last_124 - (2*pi*h_alt)*floor(X_last_124/(2*pi*h_alt));
 p_124 = sqrt(h_alt^2 + X_last_124^2);
 pe_124 = abs(p_124 - p_ref);
 
 X_last_124p5 = abs(X_plot_kmf_A124p5(end))/1000; 
-X_last_124p5 = X_last_124p5 - (2*pi*h_alt)*floor(X_last_124p5/(2*pi*h_alt));
+%X_last_124p5 = X_last_124p5 - (2*pi*h_alt)*floor(X_last_124p5/(2*pi*h_alt));
 p_124p5 = sqrt(h_alt^2 + X_last_124p5^2);
 pe_124p5 = abs(p_124p5 - p_ref);
 
 X_last_125 = abs(X_plot_kmf_A125(end))/1000; 
-X_last_125 = X_last_125 - (2*pi*h_alt)*floor(X_last_125/(2*pi*h_alt));
+%X_last_125 = X_last_125 - (2*pi*h_alt)*floor(X_last_125/(2*pi*h_alt));
 p_125 = sqrt(h_alt^2 + X_last_125^2); 
 pe_125 = abs(p_125 - p_ref);
 
 X_last_125p5 = abs(X_plot_kmf_A125p5(end))/1000; 
-X_last_125p5 = X_last_125p5 - (2*pi*h_alt)*floor(X_last_125p5/(2*pi*h_alt));
+%X_last_125p5 = X_last_125p5 - (2*pi*h_alt)*floor(X_last_125p5/(2*pi*h_alt));
 p_125p5 = sqrt(h_alt^2 + X_last_125p5^2);
 pe_125p5 = abs(p_125p5 - p_ref);
 % 
 X_last_126 = abs(X_plot_kmf_A126(end))/1000; 
-X_last_126 = X_last_126 - (2*pi*h_alt)*floor(X_last_126/(2*pi*h_alt));
+%X_last_126 = X_last_126 - (2*pi*h_alt)*floor(X_last_126/(2*pi*h_alt));
 p_126 = sqrt(h_alt^2 + X_last_126^2);
 pe_126 = abs(p_126 - p_ref);
 
+X_last_126p5 = abs(X_plot_kmf_A126p5(end))/1000; 
+%X_last_126p5 = X_last_126p5 - (2*pi*h_alt)*floor(X_last_126p5/(2*pi*h_alt));
+p_126p5 = sqrt(h_alt^2 + X_last_126p5^2);
+pe_126p5 = abs(p_126p5 - p_ref);
+
+X_last_127 = abs(X_plot_kmf_A127(end))/1000; 
+%X_last_127 = X_last_127 - (2*pi*h_alt)*floor(X_last_127/(2*pi*h_alt));
+p_127 = sqrt(h_alt^2 + X_last_127^2);
+pe_127 = abs(p_127 - p_ref);
+
+X_last_127p5 = abs(X_plot_kmf_A127p5(end))/1000; 
+%X_last_127p5 = X_last_127p5 - (2*pi*h_alt)*floor(X_last_127p5/(2*pi*h_alt));
+p_127p5 = sqrt(h_alt^2 + X_last_127p5^2);
+pe_127p5 = abs(p_127p5 - p_ref);
+
+X_last_128 = abs(X_plot_kmf_A128(end))/1000; 
+%X_last_128 = X_last_128 - (2*pi*h_alt)*floor(X_last_128/(2*pi*h_alt));
+p_128 = sqrt(h_alt^2 + X_last_128^2);
+pe_128 = abs(p_128 - p_ref);
+
 % Define the range of areas in cm^2
-areas = [23, 23.5, 24, 24.5, 25, 25.5, 26];
+areas = [23, 23.5, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28];
 
 % Define the pe values
-pe = [pe_123, pe_123p5, pe_124, pe_124p5, pe_125,pe_125p5, pe_126];%, pe_125p5, pe_126];
-
+pe = [pe_123, pe_123p5, pe_124, pe_124p5, pe_125,pe_125p5, pe_126, pe_126p5,pe_127,pe_127p5, pe_128 ];%, pe_125p5, pe_126];
+pe = pe * 100; 
 % Create a table
-T = table(areas', pe', 'VariableNames', {'Area (cm^2)', 'pe Value'});
+T = table(areas', pe', 'VariableNames', {'Area (cm^2)', 'Position Error (km)'});
 
 % Display the table
 disp(T);
 
+%%
+plot(areas,pe,'*-')
+xlabel('Perturbation Area (cm^2)');
+title('Perturbation test - Position Error')
+ylabel('Position Error (km)')
 
